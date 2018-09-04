f3e39bcbcec0e83053a0db6bc3cb20269e96b780
ShelbyBoss <42971547+ShelbyBoss@users.noreply.github.com>
2018-09-04 20:08:08 +0200

    Initial commit

---
+++ Zahlensysteme umrechnen/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Zahlensysteme_umrechnen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_von = new System.Windows.Forms.Label();
+            this.lbl_von_sys = new System.Windows.Forms.Label();
+            this.labl_von_zahl = new System.Windows.Forms.Label();
+            this.lbl_zu = new System.Windows.Forms.Label();
+            this.lbl_zu_sys = new System.Windows.Forms.Label();
+            this.lbl_zu_zahl = new System.Windows.Forms.Label();
+            this.txb_von = new System.Windows.Forms.TextBox();
+            this.txb_zu = new System.Windows.Forms.TextBox();
+            this.dud_von = new System.Windows.Forms.DomainUpDown();
+            this.dud_zu = new System.Windows.Forms.DomainUpDown();
+            this.lbl_fehler = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl_von
+            // 
+            this.lbl_von.AutoSize = true;
+            this.lbl_von.Location = new System.Drawing.Point(25, 22);
+            this.lbl_von.Name = "lbl_von";
+            this.lbl_von.Size = new System.Drawing.Size(26, 13);
+            this.lbl_von.TabIndex = 0;
+            this.lbl_von.Text = "Von";
+            // 
+            // lbl_von_sys
+            // 
+            this.lbl_von_sys.AutoSize = true;
+            this.lbl_von_sys.Location = new System.Drawing.Point(28, 55);
+            this.lbl_von_sys.Name = "lbl_von_sys";
+            this.lbl_von_sys.Size = new System.Drawing.Size(72, 13);
+            this.lbl_von_sys.TabIndex = 1;
+            this.lbl_von_sys.Text = "Zahlensystem";
+            // 
+            // labl_von_zahl
+            // 
+            this.labl_von_zahl.AutoSize = true;
+            this.labl_von_zahl.Location = new System.Drawing.Point(130, 55);
+            this.labl_von_zahl.Name = "labl_von_zahl";
+            this.labl_von_zahl.Size = new System.Drawing.Size(28, 13);
+            this.labl_von_zahl.TabIndex = 2;
+            this.labl_von_zahl.Text = "Zahl";
+            // 
+            // lbl_zu
+            // 
+            this.lbl_zu.AutoSize = true;
+            this.lbl_zu.Location = new System.Drawing.Point(28, 131);
+            this.lbl_zu.Name = "lbl_zu";
+            this.lbl_zu.Size = new System.Drawing.Size(20, 13);
+            this.lbl_zu.TabIndex = 3;
+            this.lbl_zu.Text = "Zu";
+            // 
+            // lbl_zu_sys
+            // 
+            this.lbl_zu_sys.AutoSize = true;
+            this.lbl_zu_sys.Location = new System.Drawing.Point(31, 166);
+            this.lbl_zu_sys.Name = "lbl_zu_sys";
+            this.lbl_zu_sys.Size = new System.Drawing.Size(72, 13);
+            this.lbl_zu_sys.TabIndex = 4;
+            this.lbl_zu_sys.Text = "Zahlensystem";
+            // 
+            // lbl_zu_zahl
+            // 
+            this.lbl_zu_zahl.AutoSize = true;
+            this.lbl_zu_zahl.Location = new System.Drawing.Point(130, 166);
+            this.lbl_zu_zahl.Name = "lbl_zu_zahl";
+            this.lbl_zu_zahl.Size = new System.Drawing.Size(28, 13);
+            this.lbl_zu_zahl.TabIndex = 5;
+            this.lbl_zu_zahl.Text = "Zahl";
+            // 
+            // txb_von
+            // 
+            this.txb_von.Location = new System.Drawing.Point(133, 72);
+            this.txb_von.Name = "txb_von";
+            this.txb_von.Size = new System.Drawing.Size(131, 20);
+            this.txb_von.TabIndex = 6;
+            this.txb_von.TextChanged += new System.EventHandler(this.txb_von_TextChanged);
+            // 
+            // txb_zu
+            // 
+            this.txb_zu.Enabled = false;
+            this.txb_zu.Location = new System.Drawing.Point(133, 183);
+            this.txb_zu.Name = "txb_zu";
+            this.txb_zu.Size = new System.Drawing.Size(131, 20);
+            this.txb_zu.TabIndex = 7;
+            // 
+            // dud_von
+            // 
+            this.dud_von.Items.Add("16");
+            this.dud_von.Items.Add("15");
+            this.dud_von.Items.Add("14");
+            this.dud_von.Items.Add("13");
+            this.dud_von.Items.Add("12");
+            this.dud_von.Items.Add("11");
+            this.dud_von.Items.Add("10");
+            this.dud_von.Items.Add("9");
+            this.dud_von.Items.Add("8");
+            this.dud_von.Items.Add("7");
+            this.dud_von.Items.Add("6");
+            this.dud_von.Items.Add("5");
+            this.dud_von.Items.Add("4");
+            this.dud_von.Items.Add("3");
+            this.dud_von.Items.Add("2");
+            this.dud_von.Location = new System.Drawing.Point(31, 72);
+            this.dud_von.Name = "dud_von";
+            this.dud_von.Size = new System.Drawing.Size(69, 20);
+            this.dud_von.TabIndex = 8;
+            this.dud_von.TextChanged += new System.EventHandler(this.dud_von_TextChanged);
+            // 
+            // dud_zu
+            // 
+            this.dud_zu.Items.Add("16");
+            this.dud_zu.Items.Add("15");
+            this.dud_zu.Items.Add("14");
+            this.dud_zu.Items.Add("13");
+            this.dud_zu.Items.Add("12");
+            this.dud_zu.Items.Add("11");
+            this.dud_zu.Items.Add("10");
+            this.dud_zu.Items.Add("9");
+            this.dud_zu.Items.Add("8");
+            this.dud_zu.Items.Add("7");
+            this.dud_zu.Items.Add("6");
+            this.dud_zu.Items.Add("5");
+            this.dud_zu.Items.Add("4");
+            this.dud_zu.Items.Add("3");
+            this.dud_zu.Items.Add("2");
+            this.dud_zu.Location = new System.Drawing.Point(34, 183);
+            this.dud_zu.Name = "dud_zu";
+            this.dud_zu.Size = new System.Drawing.Size(66, 20);
+            this.dud_zu.TabIndex = 9;
+            this.dud_zu.TextChanged += new System.EventHandler(this.dud_zu_TextChanged);
+            // 
+            // lbl_fehler
+            // 
+            this.lbl_fehler.ForeColor = System.Drawing.Color.Red;
+            this.lbl_fehler.Location = new System.Drawing.Point(1, 217);
+            this.lbl_fehler.Name = "lbl_fehler";
+            this.lbl_fehler.Size = new System.Drawing.Size(283, 23);
+            this.lbl_fehler.TabIndex = 10;
+            this.lbl_fehler.Text = "Fehler";
+            this.lbl_fehler.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lbl_fehler.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.lbl_fehler);
+            this.Controls.Add(this.dud_zu);
+            this.Controls.Add(this.dud_von);
+            this.Controls.Add(this.txb_zu);
+            this.Controls.Add(this.txb_von);
+            this.Controls.Add(this.lbl_zu_zahl);
+            this.Controls.Add(this.lbl_zu_sys);
+            this.Controls.Add(this.lbl_zu);
+            this.Controls.Add(this.labl_von_zahl);
+            this.Controls.Add(this.lbl_von_sys);
+            this.Controls.Add(this.lbl_von);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Zahlensystem umrechnen";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_von;
+        private System.Windows.Forms.Label lbl_von_sys;
+        private System.Windows.Forms.Label labl_von_zahl;
+        private System.Windows.Forms.Label lbl_zu;
+        private System.Windows.Forms.Label lbl_zu_sys;
+        private System.Windows.Forms.Label lbl_zu_zahl;
+        private System.Windows.Forms.TextBox txb_von;
+        private System.Windows.Forms.TextBox txb_zu;
+        private System.Windows.Forms.DomainUpDown dud_von;
+        private System.Windows.Forms.DomainUpDown dud_zu;
+        private System.Windows.Forms.Label lbl_fehler;
+    }
+}
+
---
+++ Zahlensysteme umrechnen/Form1.cs
@@ -0,0 +1,350 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Zahlensysteme_umrechnen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+                
+        private void dud_von_TextChanged(object sender, EventArgs e)
+        {
+            int zahl = 0;
+
+            try
+            {
+                zahl = Convert.ToInt32(dud_von.Text);
+
+                dud_von.Text = Convert.ToString(zahl);
+            }
+            catch
+            {
+                lbl_fehler.Text = "Eingangszahlensystem nicht möglich";
+                lbl_fehler.Visible = true;
+
+                txb_zu.Text = "Problem";
+            }
+
+            if (zahl > 16 || zahl < 2)
+            {
+                lbl_fehler.Text = "Eingangszahlensystem nicht möglich";
+                lbl_fehler.Visible = true;
+
+                txb_zu.Text = "Problem";
+            }
+            else
+            {
+                berechnen();
+            }
+        }
+
+        private void dud_zu_TextChanged(object sender, EventArgs e)
+        {
+            int zahl = 0;
+
+            try
+            {
+                zahl = Convert.ToInt32(dud_zu.Text);
+
+                dud_zu.Text = Convert.ToString(zahl);
+            }
+            catch
+            {
+                lbl_fehler.Text = "Ausgangszahlensystem nicht möglich";
+                lbl_fehler.Visible = true;
+
+                txb_zu.Text = "Problem";
+            }
+
+            if (zahl > 16 || zahl < 2)
+            {
+                lbl_fehler.Text = "Ausgangszahlensystem nicht möglich";
+                lbl_fehler.Visible = true;
+
+                txb_zu.Text = "Problem";
+            }
+            else
+            {
+                berechnen();
+            }
+        }
+
+        private void berechnen()
+        {
+            if (dud_von.Text != "" && dud_zu.Text != "")
+            {
+                int von_sys, zu_sys, zwisch = 0, i, j, x = 0, y, zusatz_1 = 0, zusatz_2 = 0;
+                int[] von_zahl = new int[20];
+                int[] wert = new int[16] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+                double größe;
+                bool fehler = false, schalt = true, groß = false, groß_von = false;
+                string test, ein, ein_o, aus = "";
+                string[] zeichen = new string[16] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F" };
+                string[] geschrieben = new string[15] { "zwei", "drei", "vier", "fünf", "sechs", "sieben", "acht", "neun", "zehn", "elf", "zwölf", "dreizehn", "vierzehn", "fünfzehn", "sechzehn" };
+                string[] zu_zahl = new string[20];
+                char[] sechszehn = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+
+                von_sys = Convert.ToInt32(dud_von.Text);
+                zu_sys = Convert.ToInt32(dud_zu.Text);
+                ein_o = txb_von.Text;
+                ein = ein_o;
+                test = ein_o;
+
+                for (i = 0; schalt; i++)
+                {
+                    for (j = 0; j < von_sys; j++)
+                    {
+                        test = test.TrimStart(sechszehn[j]);
+                    }
+
+                    if (test == ein)
+                    {
+                        schalt = false;
+                        fehler = true;
+                    }
+                    else
+                    {
+                        ein = test;
+                    }
+
+                    if (test == "")
+                    {
+                        schalt = false;
+                    }
+                }
+                
+                ein = ein_o;
+
+                if (ein != "")
+                {
+                    test = ein;
+
+                    i = 0;
+
+                    do
+                    {
+                        i++;
+
+                        test = test.Remove(0, 1);
+
+                    } while (test != "");
+                }
+                else
+                {
+                    fehler = true;
+                }
+
+                if (!fehler && i <= 20)
+                {
+                    lbl_fehler.Visible = false;
+
+                    test = ein;
+
+                    i = -1;
+
+                    do
+                    {
+                        i++;
+
+                        test = test.Remove(0, 1);
+
+                    } while (test != "");
+                                                                 
+                    if (i != 0)
+                    {
+                        for (y = 0; y <= i; y++)
+                        {
+                            test = ein.Remove(0, (i - y));
+                            test = test.Remove(1, y);
+
+                            for (j = 0; j < von_sys; j++)
+                            {
+                                if (test == zeichen[j])
+                                {
+                                    von_zahl[y] = j;
+                                }
+                            }
+                        }
+                    }                        
+                    else
+                    {
+                        for (j = 0; j < von_sys; j++)
+                        {
+                            if (ein == zeichen[j])
+                            {
+                                von_zahl[0] = j;
+                            }
+                        }
+                    }
+
+                if(!groß_von)
+                {
+                    for (y = 0; y <= i; y++)
+                    {
+                        zusatz_1 = x;
+                        
+                        if (y == 0)
+                        {
+                            x = 1;
+                        }
+                        else
+                        {
+                            x = x * von_sys;
+
+                            zusatz_2 = x;
+                            zusatz_1 = zusatz_2 / zusatz_1;
+
+                            if (zusatz_1 != von_sys)
+                            {
+                                groß = true;
+                            }
+                        }
+
+                        größe = zwisch + von_zahl[y] * x;
+
+                        if (größe >= 0 && größe < 2147483647 && x > 0)
+                        {
+                            zusatz_1 = von_zahl[y] * x;
+                            zusatz_2 = zusatz_1 / x;
+
+                            if (zusatz_2 == von_zahl[y])
+                            {
+                                zwisch = zwisch + zusatz_1;
+                            }
+                            else
+                            {
+                                groß = true;
+                            }
+                            
+                        }
+                        else
+                        {
+                            groß = true;
+                        }
+                    }
+                }
+                else
+                {
+                    größe = Convert.ToDouble(ein);
+                    
+                    if (größe <= 2147483647)
+                    {
+                        zwisch = Convert.ToInt32(ein);
+                    }
+                    else 
+                    {
+                        groß = true;
+                    }
+                }
+
+                if (!groß && !groß_von)
+                {
+                    if (zwisch != 0)
+                    {
+                        try
+                        {                            
+                            for (i = 0; zwisch != 0; i++)
+                            {
+                                zu_zahl[i] = Convert.ToString(zwisch % zu_sys);
+                                
+                                if (Convert.ToInt32(zu_zahl[i]) >= 10)
+                                {
+                                    schalt = true;
+                                    
+                                    for (j = 10; schalt; j++)
+                                    {
+                                        if (Convert.ToInt32(zu_zahl[i]) == j)
+                                        {
+                                           zu_zahl[i] = zeichen[j];
+                                            
+                                            schalt = false;
+                                        }
+                                    }
+                                }
+
+                                zwisch = zwisch / zu_sys;
+                            }
+                        }
+                        catch
+                        {
+                            aus = "Problem";
+                            
+                            lbl_fehler.Text = "Ausgangszahlensystem zu klein";
+                            lbl_fehler.Visible = true;
+
+                            i = 0;
+                        }
+                    }
+                    else
+                    {
+                        zu_zahl[0] = "0";
+
+                        i = 1;
+                    }
+                                               
+                    for (j = 0; j != i; j++)
+                    {
+                        aus = zu_zahl[j] + aus;
+                    }
+                       
+                }
+                else
+                {
+                    if (!groß)
+                    {
+                        aus = Convert.ToString(zwisch);
+                    }
+                    else
+                    {
+                        aus = "Problem";
+                        
+                        lbl_fehler.Text = "Zahl zu groß";
+                        lbl_fehler.Visible = true;                          
+                    }
+                }
+
+                txb_zu.Text = aus;
+                    
+                }
+                else
+                {
+                    txb_zu.Text = "Problem";
+
+                    if (fehler)
+                    {
+                        lbl_fehler.Text = "Zahl kann nicht umgerechnet werden";
+                    }
+                    else
+                    {
+                        lbl_fehler.Text = "Zahl zu lang";
+                    } 
+                    
+                    lbl_fehler.Visible = true;
+                }
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dud_von.Text = "10";
+            dud_zu.Text = "10";
+        }
+
+        private void txb_von_TextChanged(object sender, EventArgs e)
+        {
+            berechnen();
+
+        }
+    }
+}
